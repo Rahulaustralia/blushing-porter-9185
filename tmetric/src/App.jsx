07db340e3ab0d6e8073e5c7a26a69f0f9ddfa2df
Rahulaustralia <rahul090897pandey@gmail.com>
2022-12-16 22:36:25 +0530

    add

--- tmetric/src/App.jsx
+++ tmetric/src/App.jsx
@@ -1,4 +1,5 @@
 
+import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
 import AllRoutes from "./routes/AllRoutes";
 
@@ -6,6 +7,7 @@ function App() {
   return <div>
    <Navbar />
    <AllRoutes />
+   
   </div>;
 }
 
