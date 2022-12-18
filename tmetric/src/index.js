43536a1dbef3d270d97c3913d32da67d30c6d03e
Rahulaustralia <106013673+Rahulaustralia@users.noreply.github.com>
2022-12-19 03:51:32 +0530

    Update index.js

--- tmetric/src/index.js
+++ tmetric/src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {ChakraProvider} from "@chakra-ui/react"
 import {BrowserRouter} from "react-router-dom"
+import { Auth0Provider } from "@auth0/auth0-react";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
